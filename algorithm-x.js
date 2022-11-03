fd7b0a4de6222e76cb09f337f8ab19c00198bc9e
Joe C <52496811+Jcorb08@users.noreply.github.com>
2022-11-03 12:50:44 +0000

    buildBoard Progress

--- algorithm-x.js
+++ algorithm-x.js
@@ -2,7 +2,11 @@ class Shape{
     constructor(id,coords){
         // the letter A-L
         this.id = id;
+        // represented as 0-11 for the algorithmX array
+        this.arrayID = 55 + (this.id.charCodeAt(0) - 65); // -'A' to get 0
         // define coords here how you said rob, the pattern in a 1d array
+        // represented as 11110001
+        // a list of them rotated clockwise
         this.coords = coords;
     }
 }
@@ -16,6 +20,7 @@ class Board {
         this.shapes = [];
         //add shapes to the array
         this.createShapes();
+        this.IDColumns = this.shapes.length;
         //build basic board and store in full board, this doesn't change so we can rapid reset
         this.fullBoard = this.buildBoard();
         // the board we will change
@@ -26,10 +31,30 @@ class Board {
 
     createShapes(){
         //create the objects
+        this.shapes.push(new Shape('A',[[1,1,1,0,0,0,0,0,0,0,0,1,0,1],[1,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1],
+                                        [1,0,1,0,0,0,0,0,0,0,0,1,1,1],[1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1]]));
+        
     }
 
     buildBoard(){
         //build the board i.e. al x matrix
+        // 5 * 11 + 12 = 55 + 12 = 67 columns
+        var matrix = new Array();
+        var oneRow = new Array((this.x * this.y) + this.IDColumns).fill(0);
+        this.shapes.forEach(shape => {
+            var allPlaced = false;
+            var rotationCount = 0;
+            var colCount = 0;
+            while(!allPlaced){
+                var tempRow = oneRow;
+                tempRow[shape.arrayID] = '1';
+                Array.prototype.splice.apply(tempRow,[colCount,shape.coords[rotationCount].length].concat(shape.coords[rotationCount]));
+                matrix.push(tempRow); //push to the matrix
+                colCount++;
+                //when hits 55
+
+            }
+        });
     }
 
     prePlace(prePlace){
