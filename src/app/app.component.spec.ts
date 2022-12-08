a5da692f6b2872cd5ffbbf5b3af97c1879b33d79
Adikad10 <adi10894@gmail.com>
2022-12-08 14:02:51 +0000

    test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('frontend-polysphere-puzzle');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend-polysphere-puzzle app is running!');
-  });
+ 
 });
